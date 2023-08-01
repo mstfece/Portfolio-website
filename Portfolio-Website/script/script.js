e4f58474ab5a44cd0e9816f6f0b55e7952caad1f
Mustafa ECE <82402952+mstfece@users.noreply.github.com>
2023-08-01 23:24:54 +0300

    updating js

--- Portfolio-Website/script/script.js
+++ Portfolio-Website/script/script.js
@@ -1,9 +1,9 @@
 // add class navbarDark on navbar scroll
 const header = document.querySelector('.navbar');
 console.log(header)
-window.onscroll = function() {
+window.onscroll = function () {
     const top = window.scrollY;
-    if(top >=100) {
+    if (top >= 100) {
         header.classList.add('navbarDark');
     }
     else {
@@ -17,3 +17,60 @@ const menuToggle = document.getElementById('navbarSupportedContent')
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 })
+
+let routes = {};
+let templates = {};
+let app_div = document.getElementById('app');
+
+function EN() {
+    let div = document.createElement('div');
+    let link = document.createElement('a');
+    link.href = '#/en';
+    link.innerText = 'EN';
+
+    div.innerHTML = 'EN';
+    div.appendChild(link);
+
+    app_div.appendChild(div);
+};
+function TR() {
+    let div = document.createElement('div');
+    let link = document.createElement('a');
+    link.href = '#/';
+    link.innerText = 'EN';
+
+    div.innerHTML = 'TR';
+    div.appendChild(link);
+
+    app_div.appendChild(div);
+};
+function route(path, template) {
+    if (typeof template === 'function') {
+        return routes[path] = template;
+    }
+    else if (typeof template === 'string') {
+        return routes[path] = templates[template];
+    } else {
+        return;
+    };
+};
+function template(name, templateFunction) {
+    return templates[name] = templateFunction;
+    template('EN', function () {
+        EN();
+    });
+
+    template('TR', function () {
+        TR();
+    });
+    route('/', 'EN');
+    route('/TR', 'TR');
+};
+function router(evt) {
+    let url = window.location.hash.slice(1) || '/';
+    let route = resolveRoute(url);
+
+    route();
+};
+window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
